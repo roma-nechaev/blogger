b6c85978be5169ca46d192405ae5d9a610275490
Roman Nechaev <roma.nechayev@gmail.com>
2020-11-24 12:38:15 +0300

    index

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,8 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import Home from '../views/Home.vue';
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
 const routes = [
   {
@@ -11,19 +11,19 @@ const routes = [
     component: Home
   },
   {
-    path: '/about',
-    name: 'About',
+    path: '/index',
+    name: 'index',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    component: () => import(/* webpackChunkName: "about" */ '../views/Index.vue')
   }
-]
+];
 
 const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
   routes
-})
+});
 
-export default router
+export default router;
---
+++ src/views/Index.vue
@@ -0,0 +1,71 @@
+<template>
+  <div class="index">
+    <b-container>
+      <b-row align-v="center">
+        <b-col lg="6" class="text-center">
+          <router-link to="/">
+            <b-img src="/img/designet.png" fluid alt="" />
+          </router-link>
+          <b-button type="submit" variant="outline-primary" to="/">I have a blog</b-button>
+        </b-col>
+        <b-col lg="6" class="text-center">
+          <router-link to="/">
+            <b-img src="/img/buisness-index.png" fluid alt="" />
+          </router-link>
+          <b-button type="submit" variant="outline-primary" to="/">I have a business</b-button>
+        </b-col>
+      </b-row>
+    </b-container>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      show: true
+    };
+  }
+};
+</script>
+<style lang="scss">
+.index {
+  height: 100%;
+  .container,
+  .row {
+    height: 100%;
+  }
+  img {
+    margin: 0 50px;
+    height: 0;
+    transition: all 0.7s;
+  }
+  .btn-outline-primary {
+    font-weight: 400;
+    margin-top: 40px;
+    border-color: #5a5a5a;
+    color: #5a5a5a;
+    padding: 0.9rem 70px;
+    &:hover {
+      border-color: transparent;
+      background: linear-gradient(90deg, #f093fb 0%, #f5576c 100%);
+      color: #fff;
+    }
+  }
+  .text-center:hover img {
+    height: 100%;
+    transition: all 0.7s;
+  }
+}
+
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.7s;
+}
+.fade-enter,
+.fade-leave-to {
+  opacity: 0;
+}
+.fade-move {
+  transition: transform 1s;
+}
+</style>
